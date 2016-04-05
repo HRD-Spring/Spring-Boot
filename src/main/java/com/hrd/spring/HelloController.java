8c48634003b51ca969fc7bbda5c6454cdfc6963b
kokpheng <yinkokpheng@yahoo.com>
2016-04-06 01:29:12 +0700

    new

--- src/main/java/com/hrd/spring/HelloController.java
+++ src/main/java/com/hrd/spring/HelloController.java
@@ -2,8 +2,11 @@ package com.hrd.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @EnableAutoConfiguration
@@ -15,8 +18,17 @@ public class HelloController {
 	public String hello() {
 		return "hello";
 	}
-
+	
+	@RequestMapping(value = "", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public String greeting(@RequestBody String name) {
+		return "hello " + name;
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(HelloController.class, args);
 	}
+	
+
+
 }
